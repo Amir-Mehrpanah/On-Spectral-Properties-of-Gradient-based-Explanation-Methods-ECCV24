21f3ff027ed6d504c80c7c43610c47ab4bdd9414
Amir <amirmehrpanah@gmail.com>
2023-09-01 12:12:05 +0000

    no new bug :)

--- source/data_manager.py
+++ source/data_manager.py
@@ -31,15 +31,15 @@ def sum_channels(x):
 
 def query_imagenet(args):
     dataset = tfds.folder_dataset.ImageFolder(root_dir=args.dataset_dir)
-    dataset = dataset.as_dataset(split="val", shuffle_files=False, batch_size=1)
+    dataset = dataset.as_dataset(split="val", shuffle_files=False)
     dataset = dataset.skip(args.image_index)
     base_stream = dataset.take(1).as_numpy_iterator().next()
 
     image_height = args.input_shape[1]  # (N, H, W, C)
     base_stream["image"] = preprocess(base_stream["image"], image_height)
 
-    args.image = base_stream["image"][0]
-    args.label = base_stream["label"][0]
-    args.image_path = base_stream["image/filename"][0].decode()
-    
+    args.image = base_stream["image"]
+    args.label = base_stream["label"]
+    args.image_path = base_stream["image/filename"].decode()
+
     assert args.image.shape == args.input_shape
