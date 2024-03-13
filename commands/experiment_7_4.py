a082118e236ae0d9e75d71239dff7f4db9d4e5d6
Amir Mehrpanah <amirmehrpanah@gmail.com>
2024-03-13 09:00:18 +0100

    adding a new dataset

--- commands/experiment_7_4.py
+++ commands/experiment_7_4.py
@@ -10,8 +10,8 @@ from commands.experiment_7 import (
 )
 import commands.experiment_7
 
-alphas = " ".join([str(x / 100) for x in range(0, 101)])
-# alphas = "0.0 0.1 0.2 0.3 0.4 0.5 0.6"
+# alphas = " ".join([str(x / 100) for x in range(0, 101)])
+alphas = "0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0"
 
 commands.experiment_7.alpha_mask_value = alphas
 
@@ -32,15 +32,17 @@ commands.experiment_7.ig_alpha_priors = {  # DEBUG
     # "ig_sg_u_x2_0.2": "0.2",
     # "ig_sg_u_x2_0.3": "0.3",
     # "ig_sg_u_x2_0.4": "0.4",
-    "ig_sg_u_x2_0.5": "0.5",
+    # "ig_sg_u_x2_0.5": "0.5",
     # "ig_sg_u_x2_0.6": "0.6",
     # "ig_sg_u_x2_1.0": "1.0",
-
+    "ig_u_x_0_0.3": "0.0 0.1 0.2 0.3",
+    "ig_u_x_0_0.6": "0.0 0.1 0.2 0.3 0.4 0.5 0.6",
+    "ig_u_x_0_1.0": "0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0",
     # "ig_sg_u_x2_1.0": "1.0",
     # "ig_sg_u_x_0_0.2": "0.0 0.1 0.2",
     # "ig_sg_u_x_0_0.5": "0.0 0.1 0.2 0.3 0.4 0.5",
-    "ig_sg_u_x2_0_1.0": alphas,
-    "al_sg_u_x2_0_1.0": alphas,
+    # "ig_sg_u_x2_0_1.0": alphas,
+    # "al_sg_u_x2_0_1.0": alphas,
     # "sl_sg_u_x2_0_1.0": alphas,
     # "ig_sg_u_x2_0_0.2": "0.0 0.1 0.2",
     # "ig_sg_u_x2_0_0.5": "0.0 0.1 0.2 0.3 0.4 0.5",
@@ -53,15 +55,15 @@ commands.experiment_7.ig_alpha_priors = {  # DEBUG
     # "ig_sg_i_u_x2_0_1.0": "0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0",
 }
 commands.experiment_7.combination_fns = [
-    "additive",
+    "damping",
 ]
 
-commands.experiment_7.baseline_mask_type = "gaussian-0.3"
+commands.experiment_7.baseline_mask_type = "static"
+commands.experiment_7.baseline_mask_value = "0.0"
 commands.experiment_7.projection_type = "prediction"
 commands.experiment_7.projection_top_k = "1"
 commands.experiment_7.gather_stats_dir_batch_size = 100
-commands.experiment_7.gather_stats_take_batch_size = 1
-commands.experiment_7.gather_stats_job_array = "7,8,10,13,26"
+commands.experiment_7.gather_stats_take_batch_size = 5
 commands.experiment_7.q_baseline_masks = [
 ]
 commands.experiment_7.q_directions = [
